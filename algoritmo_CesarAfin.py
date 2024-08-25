7e1c7d64e05428765971ff4e5e6625567f30584f
jfaundez07 <j.faundez07@ufromail.cl>
2024-08-25 19:57:39 -0400

    feat(code): Se elabora el codigo para encriptar un mensaje.

--- algoritmo_CesarAfin.py
+++ algoritmo_CesarAfin.py
@@ -1,1 +1,59 @@
-print("Algoritmo de cifrado")
+key_max_size = 26
+
+alphabet = {
+    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 
+    'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 
+    'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 
+    'X': 23, 'Y': 24, 'Z': 25
+}
+
+def getMessage() -> str:
+    print('Introduzca el mensaje:')
+    return input().upper()
+
+def getAction () -> str:
+    while True:
+        print('Seleccione una accion:' + '\n'
+              '[e] encriptar' + '\n'
+              '[d] desencriptar')
+        action = input().lower()
+        if action in ['e','d']:
+            return action
+        else:
+            print('Seleccione una accion valida')
+
+def getKey() -> int:
+    key = 0
+    while True:
+        print(f'Introduzca la clave (1-{key_max_size})')
+        key = int(input())
+        if (key >= 1 and key <= key_max_size):
+            return key
+        else:
+            print(f'Introduzca una clave valida (1-{key_max_size})')
+
+def encryptMessage(message: str, key: int) -> str: #message debe venir en mayus
+    encrypted = ''
+    for letter in message:
+        if letter in alphabet:
+            encrypted += encryptLetter(letter, key)
+        else:
+            encrypted += letter
+    return encrypted
+
+def encryptLetter(letter: str, key: int) -> str:
+    letter_value = alphabet[letter]
+    encrypted_value = (letter_value + key) % key_max_size #aplica modulo siempre, pero si es 
+    for key, value in alphabet.items():
+        if value == encrypted_value:
+            return key
+    return letter
+
+
+action = getAction()
+
+if action == 'e':
+    key = getKey()
+    message = getMessage()
+    print(encryptMessage(message, key))
+
