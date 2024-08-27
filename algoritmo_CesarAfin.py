17986c938b917f907db438e63b871059c4d366bc
jfaundez07 <j.faundez07@ufromail.cl>
2024-08-26 20:01:43 -0400

    fix: se utiliza [ f(x) = (ax + b) mod n ] en reemplazo de [ f(x) = (x + b) mod n ]

--- algoritmo_CesarAfin.py
+++ algoritmo_CesarAfin.py
@@ -1,13 +1,19 @@
-from aritmeticaModular import sumaModular, restaModular
+from aritmeticaModular import sumaModular, restaModular, coprimos, inversoModular, multiplicaciónModular
 
-key_max_size = 26
+# posicion_encriptada = (a * x + b) % n
+# posicion_desencriptada = (a^-1 * (posicion_encriptada - b)) % n
+
+
 
 alphabet = {
-    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 
-    'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 
-    'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 
-    'X': 23, 'Y': 24, 'Z': 25
+    'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 
+    'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'Ñ': 15, 'O': 16, 'P': 17, 
+    'Q': 18, 'R': 19, 'S': 20, 'T': 21, 'U': 22, 'V': 23, 'W': 24, 
+    'X': 25, 'Y': 26, 'Z': 27
 }
+
+n_max = len(alphabet)
+print(n_max)
 
 def getMessage() -> str: # solicita el mensaje a encriptar
     print('Introduzca el mensaje:')
@@ -24,36 +30,71 @@ def getAction () -> str: # solicita la accion a realizar (encriptar o desencript
         else:
             print('Seleccione una accion valida')
 
-def getKey() -> int: # solicita la clave
-    key = 0
+def getA() -> int: # solicita la constante de amplificacion
+    a = 0
     while True:
-        print(f'Introduzca la clave (1-{key_max_size})')
-        key = int(input())
-        if (key >= 1 and key <= key_max_size):
-            return key
+        print(f'Introduzca la constante de amplifiacion (a > 1, excepto 2)')
+        a = int(input())
+        if validateA(a):
+            return a
         else:
-            print(f'Introduzca una clave valida (1-{key_max_size})')
+            print(f'Introduzca una constante de amplifiacion valida (a > 1, excepto 2)')
 
-def encryptDecryptMessage(message: str, key: int, action: str) -> str: # encripta o desencripta el mensaje
+def getB() -> int: # solicita la constante de desplazamiento
+    b = 0
+    while True:
+        print(f'Introduzca la constante de desplazamiento (1-{n_max})')
+        b = int(input())
+        if (b >= 1 and b <= n_max):
+            return b
+        else:
+            print(f'Introduzca una constante de desplazamiento valida (1-{n_max})')
+
+def validateA(a: int) -> bool: # valida que el valor de a sea valido
+    if a < 1:
+        return False
+    elif not coprimos(a, n_max):
+        return False
+    return True
+    
+def encryptMessage(message: str, a: int,b: int) -> str: # encripta o desencripta el mensaje
     encrypted = ''
     for letter in message:
         if letter in alphabet:
-            encrypted += encryptLetter(letter, key, action)
+            encrypted += encryptLetter(letter, a, b)
         else:
             encrypted += letter
     return encrypted
 
-def encryptLetter(letter: str, key: int, action: str) -> str: # encripta una letra
-    letter_value = alphabet[letter]
+def encryptLetter(letter: str, a: int, b: int) -> str: # encripta una letra
+    letter_value = alphabet[letter] # letter_value  corresponde a la x en posicion = (a * x + b) % n
+    amplified_letter_value = a * letter_value # a * x
 
-    if action == 'e':
-        encrypted_value = sumaModular(letter_value, key, key_max_size) # (letter_value + key) % key_max_size
-    elif action == 'd':
-        encrypted_value = restaModular(letter_value, key, key_max_size) # (letter_value - key) % key_max_size
-
-    for key, value in alphabet.items():
+    encrypted_value = sumaModular(amplified_letter_value, b, n_max) # (amplified_letter_value + b) % n_max
+    print(encrypted_value)
+    for b, value in alphabet.items():
         if value == encrypted_value:
-            return key
+            return b
+        
+def decryptMessage(message: str, a: int,b: int) -> str: # desencripta el mensaje
+    decrypted = ''
+    for letter in message:
+        if letter in alphabet:
+            decrypted += decryptLetter(letter, a, b)
+        else:
+            decrypted += letter
+    return decrypted
+        
+def decryptLetter(letter: str, a: int, b: int) -> str: # desencripta una letra
+    letter_value = alphabet[letter] 
+    modular_inverse = inversoModular(a, n_max) 
+    difference = letter_value - b
+    encrypted_value = multiplicaciónModular(modular_inverse, difference,n_max)
+    print(encrypted_value)
+    for b, value in alphabet.items():
+        if value == encrypted_value:
+            return b
+
 
 def main():
     while True:
@@ -64,8 +105,15 @@ def main():
             break
 
         action: str = getAction()
-        key: int = getKey()
+        a: int = getA()
+        b: int = getB()
         message: str = getMessage()
-        print(encryptDecryptMessage(message, key, action))
+        
+        if action == 'e':
+            encrypted_message = encryptMessage(message, a, b)
+            print(f'Mensaje encriptado: {encrypted_message}')
+        elif action == 'd':
+            decrypted_message = decryptMessage(message, a, b)
+            print(f'Mensaje desencriptado: {decrypted_message}')
 
 main()
